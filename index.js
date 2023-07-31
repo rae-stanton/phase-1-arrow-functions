07fc343d3ed382de405f2ac186daccbd0168dc1e
Rae Stanton <raestanton93@gmail.com>
2023-07-31 15:30:02 -0700

    arrow functions
    creating arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (a, b) => {
+  return a / b;
+};
+const result = divide(2000, 100);
+
+const square = (a) => {
+  return a * a;
+}
+const result1 = square(100);
+
+const add = (x, y) => {
+  return x + y;
+};
+const result2 = add(2, 5);
